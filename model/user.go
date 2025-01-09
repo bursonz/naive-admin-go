6cc3878333f59daf2aa61ba9212c3d4a349f8f93
Burson <burson@live.cn>
2025-01-09 21:28:11 +0800

    更改行分隔符为unix & 调整空格

--- model/user.go
+++ model/user.go
@@ -8,7 +8,7 @@ type User struct {
 	ID         int       `json:"id"`
 	Username   string    `json:"username"`
 	Password   string    `json:"password"`
-	Enable     bool       `json:"enable"`
+	Enable     bool      `json:"enable"`
 	CreateTime time.Time `json:"createTime" gorm:"column:createTime"`
 	UpdateTime time.Time `json:"updateTime" gorm:"column:updateTime"`
 }
