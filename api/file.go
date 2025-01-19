569429014971deb0625922f002917b912d1a5fea
Burson <burson@live.cn>
2025-01-20 01:59:36 +0800

    添加文件上传下载

---
+++ api/file.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"time"
+)
+
+type file struct{}
+
+var File = &file{}
+
+func (file) Upload(c *gin.Context) {
+	// 处理文件名
+	filename := time.Now().Format("20060102150405") + "_" + c.Param("filename")
+	// 从请求中获取上传的文件
+	f, err := c.FormFile("file")
+	if err != nil {
+		Resp.Err(c, 20001, err.Error())
+		return
+	}
+	dst := "./uploads/"
+	// 检查并创建文件夹
+	if err := os.MkdirAll(dst, os.ModePerm); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "文件夹创建失败"})
+		return
+	}
+	// 保存文件到本地
+	if err := c.SaveUploadedFile(f, dst+filename); err != nil {
+		Resp.Err(c, 20001, err.Error())
+		return
+	} else {
+		Resp.Succ(c, filename)
+	}
+}
+func (file) Download(c *gin.Context) {
+	var filename = c.Param("filename")
+	c.File("./uploads/" + filename)
+
+}
+func (file) Delete(c *gin.Context) {
+	var filename = c.Param("filename")
+	if err := os.Remove("./uploads/" + filename); err != nil {
+		Resp.Err(c, 20001, err.Error())
+	}
+	Resp.Succ(c, filename)
+}
